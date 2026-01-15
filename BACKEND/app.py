c84788e501112ab16fa3c7096f4fdf251f1512bd
Mayankgupta1694 <mayankgupta8694@gmail.com>
2026-01-15 16:55:53 +0530

    commit

--- BACKEND/app.py
+++ BACKEND/app.py
@@ -1,16 +1,23 @@
 from flask import Flask, render_template, request, redirect, session, url_for, jsonify
 from flask_mysqldb import MySQL
+import os
+from dotenv import load_dotenv
+load_dotenv()
 import bcrypt
+
+
+
 
 app = Flask(__name__)
 app.secret_key = "secret_key_123"
 
 # -------- MYSQL CONFIG --------
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'MySQL@123'
-app.config['MYSQL_DB'] = 'task_manager'
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
 
+app.secret_key = os.getenv('SECRET_KEY')
 mysql = MySQL(app)
 
 # -------- AUTH PROTECTION --------
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-this is my first website
+# Student Task Manager
+
+A full-stack web application to manage subjects and tasks.
+
+## Features
+- User authentication
+- Subject-wise task management
+- Priority-based sorting
+- Search & date filtering
+- Progress tracking
+
+## Tech Stack
+- Python (Flask)
+- MySQL
+- HTML, CSS, JavaScript
+
+## Setup Instructions
+1. Clone repository
+2. Install dependencies
+3. Configure .env
+4. Run app.py
